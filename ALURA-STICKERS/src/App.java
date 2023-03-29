ecb4383fd473bcdab989ea24f3538a06f72969f3
Magno F. Nóbrega <magnofnobrega@outlook.com>
2023-03-28 22:19:59 -0300

    extração dos dados

--- ALURA-STICKERS/src/App.java
+++ ALURA-STICKERS/src/App.java
@@ -3,6 +3,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -19,8 +21,13 @@ public class App {
             System.out.println(body);
 
         // extrair só os dados que interessam - título - poster(imagem) - classificação            
-
             
+            var parse = new JsonParser();
+            List<Map<String, String>> listadefilme = parse.parse(body);
+
+
         //exibir e manipular os dados
+
+        
     }
 }

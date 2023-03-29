92ef101281241d0744757ac18da9970e6c1bfa0e
Magno F. Nóbrega <magnofnobrega@outlook.com>
2023-03-28 22:33:09 -0300

    exibir e manipular os dados

--- ALURA-STICKERS/src/App.java
+++ ALURA-STICKERS/src/App.java
@@ -3,8 +3,11 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.security.Key;
 import java.util.List;
 import java.util.Map;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.TitlePaneLayout;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -28,6 +31,11 @@ public class App {
 
         //exibir e manipular os dados
 
-        
+        for (Map<String,String> filme : listadefilme) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+        }
+
     }
 }

d3fe48ac8ed457d7fb51948dedbc359c8e6b618f
Magno F. Nóbrega <magnofnobrega@outlook.com>
2023-03-28 22:57:00 -0300

    primeiro desafio

--- ALURA-STICKERS/src/App.java
+++ ALURA-STICKERS/src/App.java
@@ -12,13 +12,14 @@ public class App {
 
         // fazer uma conexão http e buscar os 250 filmes tops
 
-            String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+            //String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+            String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopTVs.json";
             URI endereco = URI.create(url); 
             var client = HttpClient.newHttpClient();
             var request =  HttpRequest.newBuilder(endereco).GET().build();  
             HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
             String body = response.body();
-            System.out.println(body);
+            //System.out.println(body);
 
         // extrair só os dados que interessam - título - poster(imagem) - classificação            
             
@@ -28,11 +29,14 @@ public class App {
 
         //exibir e manipular os dados
 
-        for (Map<String,String> filme : listadefilme) {
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
+        for (int i = 0; i <3; i = i + 1) {
+            Map<String,String> filme = listadefilme.get(i); 
+                System.out.println(filme.get("title"));
+                System.out.println(filme.get("image"));
+                System.out.println(filme.get("imDbRating"));
+            
         }
+        
 
     }
 }
